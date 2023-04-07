c33514b8cef2220c1bbe2b067a7adc5f8d8b7f66
Sean Anih <sean.anih@gmail.com>
2023-04-06 23:46:18 -0400

    finished layout for Nav

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+
+function Navbar() {
+	return (
+		<nav className="">
+            Nav Muthafucka
+		</nav>
+	);
+}
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,39 @@
 /* eslint-disable react/no-unescaped-entities */
 import type { NextPage } from 'next';
-import { TypeAnimation } from 'react-type-animation';
+import Head from 'next/head';
+import Link from 'next/link';
+import { BsFillMoonStarsFill } from 'react-icons/bs';
 
 const Home: NextPage = () => {
 	return (
-		<div>
-			<span style={{ fontSize: '2em' }}>
-				<span>My name is Sean. I develop</span>{' '}
-				{/* <--- will be rendered without animation */}
-				<TypeAnimation
-					sequence={['blockchain based', 1500, 'cloud based', 1500]}
-					repeat={Infinity}
-				/>
-				applications!
-			</span>
-		</div>
+		<>
+			<Head>
+				<title>Sean Anih || My Portfolio</title>
+				<meta name='description' content='This is my portfolio.' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<div className='min-h-screen bg-white text-black px-10'>
+				<header>
+					<nav className='py-10 mb-12 flex justify-between'>
+						<h1 className='text-xl font-burtons'>Sean Anih</h1>
+						<ul className='flex items-center'>
+							<li>
+								<BsFillMoonStarsFill className='cursor-pointer text-2xl' />
+							</li>
+							<li>
+								<Link
+									className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8'
+									href={'#'}
+								>
+									Resume
+								</Link>
+							</li>
+						</ul>
+					</nav>
+				</header>
+			</div>
+		</>
 	);
 };
 
